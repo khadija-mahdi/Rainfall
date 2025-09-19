037116b58dd4d2562dccdf810d3f0726bd74aa9a
ayoub-aitouna <ayoubaitouna05@gmail.com>
2025-09-19 23:36:46 +0100

    level9 source

--- level9/source.cpp
+++ level9/source.cpp
@@ -1,56 +1,38 @@
-#include <iostream>
+#include <unistd.h>
 #include <cstring>
-#include <cstdlib>
 
-class N
-{
-private:
-    char annotation[100];
-    int value;
-
+class N {
 public:
-    N(int val) : value(val)
-    {
-        memset(annotation, 0, sizeof(annotation));
-    }
+	int nb;
+	int (N::*func)(N &);
+	char annotation[100];
 
-    virtual ~N() {}
-
-    void setAnnotation(const char *str)
-    {
-        size_t len = strlen(str);
-        memcpy(annotation, str, len);
-    }
-
-    virtual int operator+(const N &other)
-    {
-        return this->value + other.value;
-    }
-
-    int operator-(const N &other)
-    {
-        return this->value - other.value;
-    }
-
-    virtual void operator()(N *other)
-    {
-        int result = *this + *other;
-        std::cout << "Result: " << result << std::endl;
-    }
-
+	N(int val) : nb(val)
+	{
+		this->func = &N::operator+;
+	}
+	int operator+(N &right)
+	{
+		return this->nb + right.nb;
+	}
+	int operator-(N &right)
+	{
+		return this->nb - right.nb;
+	}
+	void setAnnotation(char *str)
+	{
+		memcpy(this->annotation, str, strlen(str));
+	}
 };
 
-int main(int argc, char **argv)
+int		main(int ac, char **av)
 {
-    // Check if we have at least one command line argument
-    if (argc < 2)
-        exit(1);
-    // Create two N objects
-    N *obj1 = new N(5);
-    N *obj2 = new N(6);
-    // Set annotation from command line argument
-    obj1->setAnnotation(argv[1]);
-    // Call the function operator (virtual function call through vtable)
-    (*obj1)(obj1);
-    return 0;
+	if (ac < 1)
+		_exit(1);
+
+	N *a = new N(5);
+	N *b = new N(6);
+
+	a->setAnnotation(av[1]);
+	return (b->*(b->func))(*a);
 }
